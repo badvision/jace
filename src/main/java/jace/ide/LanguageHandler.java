175484b4b300c72c08389a78795a25fc8fcc909c
Brendan Robert <brendan.robert@gmail.com>
2015-08-08 15:08:46 -0500

    Clean up method stub added for cleaning after test compilations

--- src/main/java/jace/ide/LanguageHandler.java
+++ src/main/java/jace/ide/LanguageHandler.java
@@ -3,10 +3,10 @@ package jace.ide;
 public interface LanguageHandler<T> {
 
     public String getNewDocumentContent();
-
+    
     public CompileResult<T> compile(Program program);
 
     public void execute(CompileResult<T> lastResult);
     
-    
+    public void clean(CompileResult<T> lastResult);
 }
