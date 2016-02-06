58199776a5ede6140a837cf4813f70bb589f6f2b
Brendan Robert <brendan.robert@gmail.com>
2016-02-06 16:27:03 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-#java -jar target/jace-2.0-SNAPSHOT.jar
-
-# http://stackoverflow.com/questions/9689793/cant-execute-jar-file-no-main-manifest-attribute
-#java -cp target/jace-2.0-SNAPSHOT.jar jace.JaceApplication
-
-java -jar target/jace-2.0-SNAPSHOT-jar-with-dependencies.jar
+java -jar target/Jace.jar
 
